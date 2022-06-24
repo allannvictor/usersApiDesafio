e5128765a4fdc223660f5473d8be38594b0b5598
de Menezes Santos <ardt@gft.com>
2022-06-24 14:14:08 -0300

    removing comments

--- src/main/kotlin/br/project/users/users/environment/CacheEvictConfig.kt
+++ src/main/kotlin/br/project/users/users/environment/CacheEvictConfig.kt
@@ -3,7 +3,6 @@ package br.project.users.users.environment
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.scheduling.annotation.Scheduled
 
 @Configuration
 @EnableScheduling
@@ -13,12 +12,6 @@ class CacheEvictConfig(
 
     //@Scheduled( fixedRate = 30000)
     fun clearCaches(){
-//        cacheManager.cacheNames.stream()
-//            .forEach { cacheName: String? ->
-//                cacheManager.getCache(cacheName!!)!!
-//                    .clear()
-//            }
-
         cacheManager.cacheNames.map {
             cacheManager.getCache(it)?.clear()
         }
